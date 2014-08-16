d03cc1d7bd1eedb3441a10ffe69e49f8e4919116
claudiosmweb <contato@claudiosmweb.com>
2014-08-16 16:24:03 -0300

    updated grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,8 +11,9 @@ module.exports = function( grunt ) {
 			tag: '<%= svn_settings.path %>/tags/<%= pkg.version %>',
 			trunk: '<%= svn_settings.path %>/trunk',
 			exclude: [
-				'.editorconfig',
 				'.git/',
+				'.tx/',
+				'.editorconfig',
 				'.gitignore',
 				'.jshintrc',
 				'node_modules/',
@@ -23,24 +24,90 @@ module.exports = function( grunt ) {
 			]
 		},
 
-		// rsync commands used to take the files to svn repository
-		rsync: {
-			tag: {
-				src: './',
-				dest: '<%= svn_settings.tag %>',
-				recursive: true,
-				exclude: '<%= svn_settings.exclude %>'
-			},
-			trunk: {
-				src: './',
-				dest: '<%= svn_settings.trunk %>',
-				recursive: true,
-				exclude: '<%= svn_settings.exclude %>'
+		makepot: {
+			dist: {
+				options: {
+					type: 'wp-plugin',
+					potHeaders: {
+						'report-msgid-bugs-to': 'http://wordpress.org/support/plugin/woocommerce-mercadopago/',
+						'language-team': 'LANGUAGE <EMAIL@ADDRESS>'
+					}
+				}
 			}
 		},
 
-		// shell command to commit the new version of the plugin
+		checktextdomain: {
+			options:{
+				text_domain: '<%= pkg.name %>',
+				keywords: [
+					'__:1,2d',
+					'_e:1,2d',
+					'_x:1,2c,3d',
+					'esc_html__:1,2d',
+					'esc_html_e:1,2d',
+					'esc_html_x:1,2c,3d',
+					'esc_attr__:1,2d',
+					'esc_attr_e:1,2d',
+					'esc_attr_x:1,2c,3d',
+					'_ex:1,2c,3d',
+					'_n:1,2,4d',
+					'_nx:1,2,4c,5d',
+					'_n_noop:1,2,3d',
+					'_nx_noop:1,2,3c,4d'
+				]
+			},
+			files: {
+				src:  [
+					'**/*.php', // Include all files
+					'!node_modules/**' // Exclude node_modules/
+				],
+				expand: true
+			}
+		},
+
+		po2mo: {
+			files: {
+				src: 'languages/*.po',
+				expand: true
+			}
+		},
+
+		// Rsync commands used to take the files to svn repository
+		rsync: {
+			options: {
+				args: ['--verbose'],
+				exclude: '<%= svn_settings.exclude %>',
+				syncDest: true,
+				recursive: true
+			},
+			tag: {
+				options: {
+					src: './',
+					dest: '<%= svn_settings.tag %>'
+				}
+			},
+			trunk: {
+				options: {
+				src: './',
+				dest: '<%= svn_settings.trunk %>'
+				}
+			}
+		},
+
+		// Shell command to commit the new version of the plugin
 		shell: {
+			// Remove delete files.
+			svn_remove: {
+				command: 'svn st | grep \'^!\' | awk \'{print $2}\' | xargs svn --force delete',
+				options: {
+					stdout: true,
+					stderr: true,
+					execOptions: {
+						cwd: '<%= svn_settings.path %>'
+					}
+				}
+			},
+			// Add new files.
 			svn_add: {
 				command: 'svn add --force * --auto-props --parents --depth infinity -q',
 				options: {
@@ -51,6 +118,7 @@ module.exports = function( grunt ) {
 					}
 				}
 			},
+			// Commit the changes.
 			svn_commit: {
 				command: 'svn commit -m "updated the plugin version to <%= pkg.version %>"',
 				options: {
@@ -68,11 +136,16 @@ module.exports = function( grunt ) {
 	// load tasks
 	grunt.loadNpmTasks( 'grunt-rsync' );
 	grunt.loadNpmTasks( 'grunt-shell' );
+	grunt.loadNpmTasks( 'grunt-wp-i18n' );
+	grunt.loadNpmTasks( 'grunt-checktextdomain' );
+	grunt.loadNpmTasks( 'grunt-po2mo' );
 
-	// deploy task
-	grunt.registerTask( 'default', [
+	// Deploy task
+	grunt.registerTask( 'deploy', [
+		'default',
 		'rsync:tag',
 		'rsync:trunk',
+		'shell:svn_remove',
 		'shell:svn_add',
 		'shell:svn_commit'
 	] );

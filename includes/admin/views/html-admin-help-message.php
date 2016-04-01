087520c31a52ea79729cf4b410f04f2125b47187
Claudio Sanches <contato@claudiosmweb.com>
2016-04-01 16:28:32 -0300

    Fixed inline messages

--- includes/admin/views/html-admin-help-message.php
+++ includes/admin/views/html-admin-help-message.php
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if ( apply_filters( 'woocommerce_mercadopago_help_message', true ) ) : ?>
-	<div class="updated woocommerce-message">
+	<div class="updated woocommerce-message inline">
 		<p><?php echo esc_html( sprintf( __( 'Help us keep the %s plugin free making a donation or rate %s on WordPress.org. Thank you in advance!', 'woocommerce-mercadopago' ), __( 'WooCommerce MercadoPago', 'woocommerce-mercadopago' ), '&#9733;&#9733;&#9733;&#9733;&#9733;' ) ); ?></p>
 		<p><a href="http://claudiosmweb.com/doacoes/" target="_blank" class="button button-primary"><?php esc_html_e( 'Make a donation', 'woocommerce-mercadopago' ); ?></a> <a href="https://wordpress.org/support/view/plugin-reviews/woocommerce-mercadopago?filter=5#postform" target="_blank" class="button button-secondary"><?php esc_html_e( 'Make a review', 'woocommerce-mercadopago' ); ?></a></p>
 	</div>
--- includes/admin/views/html-notice-currency-not-supported.php
+++ includes/admin/views/html-notice-currency-not-supported.php
@@ -11,7 +11,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 ?>
 
-<div class="error">
+<div class="error inline">
 	<p><strong><?php _e( 'WooCommerce MercadoPago Disabled', 'woocommerce-mercadopago' ); ?></strong>: <?php printf( __( 'Currency %s is not supported. Please make sure that you use one of the following supported currencies: %s.', 'woocommerce-mercadopago' ), '<code>' . get_woocommerce_currency() . '</code>', '<code>' . implode( '</code>, <code>', $this->api->get_supported_currencies() ) . '</code>' ); ?>
 	</p>
 </div>
